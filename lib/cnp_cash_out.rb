8e6fe677bea39f8461ad220f45a04f1ffb6a6524
Izhanov <ibekable@gmail.com>
2019-05-28 17:08:54 +0600

    Added ::StartTransaction

--- lib/cnp_cash_out.rb
+++ lib/cnp_cash_out.rb
@@ -3,6 +3,7 @@ require 'cnp_cash_out/config'
 require 'cnp_cash_out/cash_out_to_registered_card_start'
 require 'cnp_cash_out/status'
 require 'cnp_cash_out/complete'
+require 'cnp_cash_out/start_transaction'
 
 module CnpCashOut
   # Your code goes here...
