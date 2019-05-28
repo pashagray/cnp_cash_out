0f1399da51cb56f3c1ec72d386425f9aaab23aac
Izhanov <ibekable@gmail.com>
2019-05-28 15:22:56 +0600

    Added changes: 1) merchant_id = 000000000000097; 2) receiver_user_id = 21901; 3) receiver_card_id = 11588; 4) receiver_user_login = vinci@gmail.com.

--- spec/cnp_cash_out/cash_out_to_registered_card_start_spec.rb
+++ spec/cnp_cash_out/cash_out_to_registered_card_start_spec.rb
@@ -7,7 +7,7 @@ describe CnpCashOut::CashOutToRegisteredCardStart do
     before do
 
       CnpCashOut.configure(
-        merchant_id: '777000000000018',
+        merchant_id: '000000000000097',
         language_code: 'en',
         currency_code: 398,
         wsdl: 'spec/CNPMerchantWebService.wsdl',
@@ -19,14 +19,14 @@ describe CnpCashOut::CashOutToRegisteredCardStart do
       it 'raises error' do
         expect do
           described_class.new
-        end.to raise_error(StandardError).with_message('[:merchant_keyword, :tran_amount, :return_u_r_l, :merchant_local_date_time, :user_id, :card_id, :receiver_user_id, :user_login, :receiver_card_id, :sender_name] is required, but not set')
+        end.to raise_error(StandardError).with_message('[:card_id, :merchant_keyword, :merchant_local_date_time, :receiver_card_id, :receiver_user_id, :return_u_r_l, :sender_name, :tran_amount, :user_id, :user_login] is required, but not set')
       end
     end
 
     context 'when all required params are presented' do
       it 'returns url for registration new card' do
         # stub_start_card_registration_request
-        expect(described_class.new(return_u_r_l: ':3000', merchant_keyword: '111', tran_amount: 100000, merchant_local_date_time: '01.02.2012 12:34:58', user_id: 3671, card_id: 2343, receiver_user_id: 3671, receiver_card_id: 2343, user_login: 'tpepost@gmail.com', receiver_user_login: 'tpepost@gmail.com', sender_name: 'Ivan Ivanov').success).to eq(true)
+        expect(described_class.new(return_u_r_l: ':3000', merchant_keyword: '111', tran_amount: 100000, merchant_local_date_time: '01.02.2012 12:34:58', user_id: 3372, card_id: 2246, receiver_user_id: 21901, receiver_card_id: 11588, user_login: 'tpepost@gmail.com', receiver_user_login: 'vinci@gmail.com', sender_name: 'Ivan Ivanov').success).to eq(true)
       end
     end
   end
